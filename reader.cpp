d0fb828d930e16174e7980269b6918480f27014a
Vicfred <vicfredboy@gmail.com>
2019-08-08 17:59:50 -0500

    basic cmake for protobuf

--- reader.cpp
+++ reader.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include "foo.pb.h"
 
-int main() {
-    std::cout << "Olis olis" << std::endl;
-    return 0;
+int main(int argc, char** argv) {
+    lagmental::Person message;
+    message.set_name("Olis olis Protobuf");
+    std::cout << message.name() << std::endl;
 }
